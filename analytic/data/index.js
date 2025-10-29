496510865746361a7c9f6b00c93e721381c2419e
abdurahmatovhayrulla <abdurahmatovhayrulla@gmail.com>
2025-10-29 23:58:01 +0500

    update index.js

--- analytic/data/index.js
+++ analytic/data/index.js
@@ -1,3 +1,3 @@
 document.getElementById("desktop").innerHTML = "<div id='ds_top'></div>";
-document.getElementById("planshet").innerHTML = "";
+document.getElementById("planshet").innerHTML = "<div id='_top'></div>";
 document.getElementById("mobil").innerHTML = "";
